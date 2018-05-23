0a3d9a0f10008b68b3405ca8366bc4cdb19c6466
Matt Maher <mattmaher0@gmail.com>
2018-05-23 15:52:26 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,7 +5,7 @@ def load_library(file_path)
   result = {"get_meaning" = {}, "get_emoticon" = {}}
   library.each do |meaning, emoticon|
   result[get_meaning][emoticon[1]] = meaning
-  result[get_emoticon][emoticon]
+  result[get_emoticon][emoticon[0]] = emoticon
   
   
 end
